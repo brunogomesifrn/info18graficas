c4b85aef3143dfe5436b17bb6a486ca380862013
Josymar Cortez de Melo <josymar.cortez@academico.ifrn.edu.br>
2018-10-17 16:45:25 -0300

    mudei alguns links 'apple-touch-icon-precomposed'

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -19,10 +19,10 @@
   <!-- Theme skin -->
   <link href="skins/default.css" rel="stylesheet" />
   <!-- Fav and touch icons -->
-  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="ico/apple-touch-icon-144-precomposed.png" />
-  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png" />
-  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png" />
-  <link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{asset('ico/apple-touch-icon-144-precomposed.png')}}" />
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{asset('ico/apple-touch-icon-114-precomposed.png')}}" />
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{asset('ico/apple-touch-icon-72-precomposed.png')}}" />
+  <link rel="apple-touch-icon-precomposed" href="{{asset('ico/apple-touch-icon-57-precomposed.png')}}" />
   <link rel="shortcut icon" href="ico/favicon.png" />
 
   <!-- =======================================================
