dbd67a711a40f0ead7e82bcdd6599e218bbf0c07
Nayara_Soares <nayarasooares18@gmail.com>
2018-12-10 15:17:05 -0200

    salvando para cortez acessar

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -25,11 +25,6 @@
   <link rel="apple-touch-icon-precomposed" href="{{asset('ico/apple-touch-icon-57-precomposed.png')}}" />
   <link rel="shortcut icon" href="{{asset('ico/favicon.png')}}" />
 
-  <!--<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-  <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>-->
-<!------ Include the above in your HEAD tag ---------->
-
  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="#" crossorigin="anonymous">
 
 
--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -2,28 +2,10 @@
 @section('conteudo')
 
 <p><h1>Bem-vindo {{Auth::user()->name}}</h1></p>
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-3 ">
-		     <div class="list-group ">
-              <a href="/perfil" class="list-group-item list-group-item-action active">{{Auth::user()->name}}</a>
-              <a href="/grafica" class="list-group-item list-group-item-action">Minhas gráficas</a>
-              <a href="#" class="list-group-item list-group-item-action">Meu Pedidos</a>
-              <a href="#" class="list-group-item list-group-item-action">Serviços</a>
-              
-		    </div>
-		</div>
-	</div>
-</div>
-<!--<p><a href="/grafica">Minhas gráficas</a></p>
+<p><a href="/grafica">Minhas gráficas</a></p>
 <p>Meus Pedidos</p>
-
-
 <p>Serviços</p>
-<a href="#"><i class="fa fa-pencil-square-o"></i></a>
-                                        <a href="#"><i class="fa fa-eye"></i></a>
-                                        <a href="#"></a>
--->
+
 
 
 @endsection
